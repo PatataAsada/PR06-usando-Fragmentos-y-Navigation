ad0a80786ee16af1133e5d7a8fa828a54046f96f
Yeray <yeray3000@gmail.com>
2019-05-21 10:43:35 +0200

    cambios en avatarfragment

--- PR06-new/app/src/main/java/es/iessaladillo/yeraymoreno/pr06_new/ui/MainActivity/avatarFragment/AvatarViewModel.java
+++ PR06-new/app/src/main/java/es/iessaladillo/yeraymoreno/pr06_new/ui/MainActivity/avatarFragment/AvatarViewModel.java
@@ -1,4 +1,4 @@
-package es.iessaladillo.yeraymoreno.pr06_new.ui.avatarFragment;
+package es.iessaladillo.yeraymoreno.pr06_new.ui.MainActivity.avatarFragment;
 
 import androidx.lifecycle.ViewModel;
 
