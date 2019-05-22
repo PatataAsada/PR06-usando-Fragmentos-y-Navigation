2afcc86376ea7a4203257cf5fa67d5eee2a48b0c
Yeray <yeray3000@gmail.com>
2019-05-22 13:28:08 +0200

    Intent para mandar a crear student

--- PR06-new/app/src/main/java/es/iessaladillo/yeraymoreno/pr06_new/ui/mainactivity/mainFragment/MainFragment.java
+++ PR06-new/app/src/main/java/es/iessaladillo/yeraymoreno/pr06_new/ui/mainactivity/mainFragment/MainFragment.java
@@ -1,5 +1,7 @@
 package es.iessaladillo.yeraymoreno.pr06_new.ui.mainactivity.mainFragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -14,8 +16,6 @@ import androidx.core.view.ViewCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 
@@ -25,9 +25,14 @@ import es.iessaladillo.yeraymoreno.pr06_new.R;
 import es.iessaladillo.yeraymoreno.pr06_new.data.AppDatabaseStudents;
 import es.iessaladillo.yeraymoreno.pr06_new.data.model.Student;
 import es.iessaladillo.yeraymoreno.pr06_new.databinding.FragmentMainBinding;
+import es.iessaladillo.yeraymoreno.pr06_new.ui.studentactivity.StudentActivity;
 import es.iessaladillo.yeraymoreno.pr06_new.ui.studentactivity.studentFragment.StudentViewModel;
 
 public class MainFragment extends Fragment {
+
+    private static final String STUDENT = "STUDENT";
+    private static final int EDIT_STUDENT = 1;
+    private static final int ADD_STUDENT = 2;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -37,6 +42,7 @@ public class MainFragment extends Fragment {
     private StudentViewModel pViewModel;
     public MainFragmentAdapter listAdapter;
     public MainFragmentViewModel mViewModel;
+    public Intent studentIntent;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -112,10 +118,6 @@ public class MainFragment extends Fragment {
         sendIntent();
     }
 
-    private void sendIntent() {
-        //TODO Hacer intent para editar/annadir estudiante.
-    }
-
     //Sets the toolbar title.
     private void setupToolbar(View view) {
         Toolbar toolbar = ViewCompat.requireViewById(view, R.id.toolbar);
@@ -132,5 +134,29 @@ public class MainFragment extends Fragment {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendIntent() {
+        studentIntent = new Intent(getContext(), StudentActivity.class);
+        studentIntent.putExtra(STUDENT,pViewModel.getStudent());
+        if(pViewModel.isEdit){
+            startActivityForResult(studentIntent,EDIT_STUDENT);
+            onActivityResult(EDIT_STUDENT, Activity.RESULT_OK, studentIntent);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EDIT_STUDENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                mViewModel.editStudent(pViewModel.getOldStudent(),Objects.requireNonNull(data).getParcelableExtra(STUDENT));
+            }
+        }
+        if(requestCode == ADD_STUDENT){
+            if(resultCode == Activity.RESULT_OK){
+                mViewModel.addStudent(Objects.requireNonNull(data).getParcelableExtra(STUDENT));
+            }
+        }
     }
 }
